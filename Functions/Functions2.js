beb966e547cc1de384c998ff6dd46771e89a746a
Anna Glikin <glikin.anna@gmail.com>
2025-07-10 10:39:22 +0300

    Refactor JavaScript exercise: fix formatting issues in people_info array and enhance getSummary function for clarity

--- Functions/Functions2.js
+++ Functions/Functions2.js
@@ -10,7 +10,7 @@ const people_info = [
     age: 17,
     country: "canaland",
     city: "sydurn",
-    catchphrase: "what a piece of wood!"
+    catchphrase: "what a piece of wood!",
   },
   {
     name: "petra",
@@ -18,7 +18,7 @@ const people_info = [
     age: 31,
     country: "greenmark",
     city: "bostork",
-    catchphrase: "that's my engine, bub"
+    catchphrase: "that's my engine, bub",
   },
   {
     name: "damian",
@@ -26,32 +26,40 @@ const people_info = [
     age: 72,
     country: "zimbia",
     city: "bekyo",
-    catchphrase: "with great responsibility comes great power"
-  }
-]
+    catchphrase: "with great responsibility comes great power",
+  },
+];
 
-const capitalize = function(str) {
-  let capitalizedStr = ""
-  capitalizedStr += str[0].toUpperCase()      // first letter, capitalized
-  capitalizedStr += str.slice(1)              // rest of the string
-  return capitalizedStr
+const capitalize = function (str) {
+  let capitalizedStr = "";
+  capitalizedStr += str[0].toUpperCase(); // first letter, capitalized
+  capitalizedStr += str.slice(1); // rest of the string
+  return capitalizedStr;
+};
+// console.log(capitalize("hello"));
+
+function getAge(age) {
+  if (age < 21) return "an Underage";
+  else if (age >= 55) return "an 55+";
+  return `a ${age}`;
 }
-console.log(capitalize("hello"))
+// console.log(getAge(people_info[0].age));
 
-const getSummary = function(person){
-  let summary = ""
-  summary += capitalize(person.name)
-  summary += ` is ${getAge(person.age)} ` //Yes - you can put a function call inside the tick quotes!
-  summary += //call function for profession
-  summary += //call function for country + city
-  summary += //call function for catchphrase
-  return summary
-}
-console.log(getSummary(people_info[0]))
+const getSummary = function (person) {
+  let summary = "";
+  summary += capitalize(person.name);
+  summary += ` is ${getAge(person.age)} `; //Yes - you can put a function call inside the tick quotes!
+  summary += capitalize(person.profession); //call function for profession
+  summary += ` frome ${capitalize(person.city)}, ${capitalize(
+    person.country
+  )}. `; //call function for country + city
+  summary += `${capitalize(person.name)} loves to say "${capitalize(
+    person.catchphrase
+  )}".`; //call function for catchphrase
+  return summary;
+};
+// console.log(getSummary(people_info[0]));
 
-
-function getAge(arr){
-  for (let age of arr) {
-   console.log (age);
-  }
-}
+people_info.forEach((person) => {
+  console.log(getSummary(person));
+});
