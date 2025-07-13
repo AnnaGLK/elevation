ee5635b6af68b145bb3b210a5ff86b9130e02cb4
Anna Glikin <glikin.anna@gmail.com>
2025-07-13 13:19:16 +0300

    Add package.json and .gitignore; implement printDuplicates and FindSum functions

---
+++ JS/js class work/Algorithmic Question Strategies.js
@@ -0,0 +1,32 @@
+function printDuplicates(arr){
+    addEventListener.sort((a,b) => a-b);
+    for(let i=1; i<=arr.length;i++){
+        if (arr[i-1] === arr[i]){
+            console.log(arr[i])
+        }
+    }
+}
+
+
+const FindSum = function findNumSum(arr,num){
+for (let i=0; i<arr.length;i++) {
+    for ( let j=i+1; j<arr.length; j++){
+        return true;
+    }
+}return false;
+}
+
+arr=[1,4,2,67,3,5,6,3]
+console.log (FindSum(arr, 9))
+
+
+const number =[2,7,11,15];
+const target = 17;
+const twoSum = new Set()
+
+for (let num of arr) {
+    if (mySet.has){
+        return true;
+    }
+}return false;
+
